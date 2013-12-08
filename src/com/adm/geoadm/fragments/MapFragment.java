65ee82136f1ce40df7102152f12f2f4ccfb62fda
Diguin <diego.diguin91@gmail.com>
2013-12-08 20:26:35 +0100

    Añadida funcionalidad al boton go, nos lleva a la dirección que le ponemos en el campo de texto.

--- src/com/adm/geoadm/fragments/MapFragment.java
+++ src/com/adm/geoadm/fragments/MapFragment.java
@@ -1,6 +1,14 @@
 package com.adm.geoadm.fragments;
 
+import java.io.IOException;
+import java.util.List;
+
+import android.annotation.TargetApi;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,13 +16,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.adm.geoadm.R;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter;
 import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 
 public class MapFragment  extends Fragment implements InfoWindowAdapter, OnInfoWindowClickListener {
@@ -38,6 +49,7 @@ public class MapFragment  extends Fragment implements InfoWindowAdapter, OnInfoW
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_map, null);
+		etAddress = (EditText) view.findViewById(R.id.etAddress);
 		
 		if (map == null) {
 			map = ((SupportMapFragment) getFragmentManager().findFragmentById(R.id.fMap)).getMap();
@@ -49,8 +61,22 @@ public class MapFragment  extends Fragment implements InfoWindowAdapter, OnInfoW
 		
 		}
 			}
+		
+		
+		Button btGo = (Button) view.findViewById(R.id.btGo);
+		btGo.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				new MyGeoder().execute();
+
+			}
+		});
 		return view;
 	}
+	
+	
 
 	@Override
 	public void onInfoWindowClick(Marker arg0) {
@@ -68,6 +94,45 @@ public class MapFragment  extends Fragment implements InfoWindowAdapter, OnInfoW
 	public View getInfoWindow(Marker marker) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	@TargetApi(Build.VERSION_CODES.GINGERBREAD)
+	private class MyGeoder extends AsyncTask<Void, Void, Void> {
+
+		List<Address> address = null;
+
+		
+		@Override
+		protected Void doInBackground(Void... params) {
+			// TODO Auto-generated method stub
+			Geocoder geocoder = new Geocoder(getActivity());
+			try {
+				if (Geocoder.isPresent()) {
+					address = geocoder.getFromLocationName(etAddress.getText().toString(), 1);
+				}
+			} catch (NumberFormatException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(result);
+			if ((address != null) && (address.size() > 0)) {
+				LatLng lat= new LatLng(address.get(0).getLatitude(),address.get(0).getLongitude());
+				map.animateCamera(CameraUpdateFactory.newLatLngZoom(lat,17));
+			}
+			else {
+				Toast.makeText(getActivity(), "Geocoder unavailable", Toast.LENGTH_SHORT).show();
+			}
+		}
+
 	}
 	
 }
